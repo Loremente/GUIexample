5a7c74f439730be08a3be3e669dd804c9f04723b
Lorenzo <Loremente@outlook.it>
2022-02-05 14:46:51 +0100

    Main class

--- src/main/java/me/loremente/gui/GUI.java
+++ src/main/java/me/loremente/gui/GUI.java
@@ -1,5 +1,6 @@
 package me.loremente.gui;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GUI extends JavaPlugin {
@@ -7,5 +8,7 @@ public final class GUI extends JavaPlugin {
     @Override
     public void onEnable() {
         getCommand("gui").setExecutor(new MenuCommand());
+        Bukkit.getPluginManager().registerEvents(new MenuListener(), this);
+
     }
 }
